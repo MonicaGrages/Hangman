20c8d4530c876479a16b669455ccc5e201e6389b
MonicaGrages <mgrages1@gmail.com>
2017-05-09 10:46:52 -0400

    added JS pseudocoding for starting game/creating game board

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
   var gameBoard = {
+    startGame : function () {
+      // called by start button click handler
+      //should call generateRandomSecretWord and createLetterBoard
+    },
+    generateRandomSecretWord : function () {
+      //will get random secret word from word bank and
+      //add hidden letter list to game board
+    },
+    createLetterBoard : function() {
+      //will make the letter board show up after game starts
+    },
 
   };
 
@@ -11,7 +22,15 @@ $(document).ready(function() {
 
 
 
-
+var buttonHandlers = {
+  startButtonHandler : function() {
+    //event handler for start button click
+    //should call gameBoard.startGame() from here
+  },
+  resetButtonHandler : function() {
+    //event handler for reset button click
+  },
+};
 
 
 });
