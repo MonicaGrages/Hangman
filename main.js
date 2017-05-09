411118055bbcadb54f83a0fd1c44990daaa7bdea
MonicaGrages <mgrages1@gmail.com>
2017-05-09 11:07:05 -0400

    added start button click handler

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
+
+
+
   var gameBoard = {
     startGame : function () {
       // called by start button click handler
       //should call generateRandomSecretWord and createLetterBoard
+      console.log('start click');
     },
     generateRandomSecretWord : function () {
       //will get random secret word from word bank and
@@ -11,9 +15,19 @@ $(document).ready(function() {
     createLetterBoard : function() {
       //will make the letter board show up after game starts
     },
+    endGame : function () {
+      //win vs loss?
+    },
+    resetGame : function () {
+      //will be called when user clicks reset button
+    }
 
   };
 
+
+var scoreBoard = {
+
+};
 
 
 
@@ -23,14 +37,28 @@ $(document).ready(function() {
 
 
 var buttonHandlers = {
-  startButtonHandler : function() {
+  startClickHandler : function() {
     //event handler for start button click
     //should call gameBoard.startGame() from here
+    gameBoard.startGame();
   },
-  resetButtonHandler : function() {
+  resetClickHandler : function() {
     //event handler for reset button click
   },
+  letterClickHandler : function () {
+    //add event handler for letter clicks
+    //should call letter evaluating function(s)
+  }
 };
 
 
+
+
+  $('#startButton').on('click', buttonHandlers.startClickHandler);
+  //put reset button click event handler here and call resetClickHandler
+  //put letter click event handler here and call letterClickHandler
+
+
+
+//below is the end of the document.ready function
 });
